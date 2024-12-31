d151ad80906f7e6bd4ab7b03a3c853f81e00dce2
alexeei <megaxaser@gmail.com>
2024-12-31 14:34:39 +0300

    implement fastcharge

--- device.mk
+++ device.mk
@@ -206,6 +206,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 	
+# Fastcharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.xiaomi_sm8250
+
 # FOD
 TARGET_HAS_UDFPS := true
 
