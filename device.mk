9cb95e81e303ddbe308eb8c0be953db97a28d3f6
alexeei <megaxaser@gmail.com>
2025-03-04 08:39:43 +0300

    Update android.hardware.tetheroffload.control to version 1.1  (wifi hotspot crash fix )

--- device.mk
+++ device.mk
@@ -401,6 +401,11 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client 
 
+# sendhint utility
+PRODUCT_PACKAGES += \
+    sendhint
+
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
